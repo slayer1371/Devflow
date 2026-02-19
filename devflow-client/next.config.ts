dae4341fe5f496d773ca5744f307ef5d81fc04a0
Mrinal Sharma <mrinalwork2@gmail.com>
2026-02-19 00:50:41 -0500

    chore: monorepo structure and shared dependencies

--- devflow-client/next.config.ts
+++ devflow-client/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 //allow access from other devices on the network
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: "/Users/mac/Downloads/codd/devflow"
+  }
 };
 
 export default nextConfig;
